f24651fc0b15a66950316e12c96c2226cdad2873
Mikael Kökeritz <mikael.kokeritz@gmail.com>
2022-03-10 19:22:53 +0100

    Variabels now show up on the website. I am screwed

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 var Treassury = 65;
 var Population = 50;
+var Popularity = 25;
+var Controll = 50;
 
-function WriteVar() {
-  document.getElementById("WriteOut").innerHTML = number;
+const TreassuryField = document.querySelector("[data-treassury]");
+const PopulationField = document.querySelector("[data-population]");
+const PopularityField = document.querySelector("[data-popularity]");
+const ControllField = document.querySelector("[data-controll]");
+
+function FieldsUpdater() {
+  TreassuryField.innerHTML = Treassury;
+  PopulationField.innerHTML = Population;
+  PopularityField.innerHTML = Popularity;
+  ControllField.innerHTML = Controll;
 }
+
+FieldsUpdater();
