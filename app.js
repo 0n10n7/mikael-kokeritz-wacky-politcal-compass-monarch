2ccbb9ade758e44aba2af0909bfc0ed8f7ab8e97
Mikael Kökeritz <mikael.kokeritz@gmail.com>
2022-03-14 11:00:31 +0100

    Text rezising poggers

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var Population = 50;
 var Popularity = 25;
 var Controll = 50;
 
+var Xcord =0;
+var Ycord =7;
+
 const TreassuryField = document.querySelector("[data-treassury]");
 const PopulationField = document.querySelector("[data-population]");
 const PopularityField = document.querySelector("[data-popularity]");
@@ -14,5 +17,25 @@ function FieldsUpdater() {
   PopularityField.innerHTML = Popularity;
   ControllField.innerHTML = Controll;
 }
+
+const Xbox = document.querySelectorAll(".InfoHUD");
+
+function UpdateDaBoxes() {
+  Xbox.forEach(box => {
+    var Grogu = box.firstElementChild;
+    var size = 100;
+    Grogu.style.setProperty('--size', `${size}%`);
+
+    while (Grogu.clientHeight > box.clientHeight - 5 && size >0){
+      Grogu.style.setProperty('--size', `${size}%`);
+        size --;
+    }
+    
+  });
+}
+
+window.addEventListener('resize', UpdateDaBoxes);
+
+UpdateDaBoxes();
 
 FieldsUpdater();
